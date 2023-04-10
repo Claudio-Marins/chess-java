ecceb5bdda41a79c905c73fb18dc0533da89f61e
Cláudio Marins <claudiomarins001@gmail.com>
2023-04-10 09:52:35 -0300

    Created some chess pieces and created the initial setup and piece placement

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
 
 import java.security.PublicKey;
 
@@ -9,6 +11,7 @@ public class ChessMatch {
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getpieces(){
@@ -20,6 +23,10 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup(){
+        board.placePiece(new Rook(board, Color.WHITE),new Position(2,1));
     }
 
 
