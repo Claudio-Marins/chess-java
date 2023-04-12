3395f0334229fec57a3583e89688c92eda5335d8
Cláudio Marins <claudiomarins001@gmail.com>
2023-04-12 09:51:08 -0300

    Created some exceptions

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: the must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -28,6 +31,9 @@ public class Board {
     }
 
     public Piece piece(int row, int column){
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position not on the board!");
+        }
         return pieces[row][column];
 
     }
@@ -36,10 +42,25 @@ public class Board {
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("There is already a piece on this position");
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
 
     }
+
+    private boolean positionExists(int row, int column){
+       return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece (Position position){
+        return piece(position) != null;
+    }
+
 
 
 
---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,11 @@
+package boardgame;
+
+import javax.swing.text.StyledEditorKit;
+
+public class BoardException extends RuntimeException{
+    public BoardException (String msg){
+        super(msg);
+    }
+
+
+}
